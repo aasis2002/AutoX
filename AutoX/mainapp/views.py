35d79bf72f879684927b3d58387faf02a0e9328d
Aashish Mehta <120818102+aasis2002@users.noreply.github.com>
2024-04-10 02:33:10 +0530

    template rendering

--- AutoX/mainapp/views.py
+++ AutoX/mainapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def main_view(request):
-    return HttpResponse('<h1>Welcome To AutoX !!</h1>')
+    return render(request, 'view/main.html')
